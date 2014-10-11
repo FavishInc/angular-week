53c0774f8bc4676cc4cf185342dc375fe2b9a1a4
Michael Favia <michael@favish.com>
2014-10-11 14:33:41 -0500

    updates to make more angular liek

--- angular-week.js
+++ angular-week.js
@@ -7,20 +7,31 @@
  * # ngWeek
  */
 angular.module('angularWeek', [])
-  .service('angularWeek', function ngWeek() {
+  .factory('angularWeek', function () {
+    var angularWeek = function(date){
+       //this.setDate(date);
+    };
 
-    // Stub out a blank function for the singleton.
-    var angularWeek = function(){};
-    angularWeek.fetch = function (date) {
-      return [moment(date).weekday(0), moment(date).weekday(6)];
+    angularWeek.prototype.setStart = function (date) {
+        this.startDate = moment(date).weekday(0);
+        this.endDate   = moment(date).weekday(6);
     }
-    angularWeek.nextWeek = function (week) {
-      return angularWeek.fetch(week[1].add(7, 'days'));
+    angularWeek.prototype.next = function () {
+      return this.startDate.add(7, 'days');
     }
-    angularWeek.prevWeek = function (week) {
-      return angularWeek.fetch(week[1].subtract(7, 'days'));
+    angularWeek.prototype.prev = function (week) {
+      return this.endDate.subtract(7, 'days');
     }
-
-    return angularWeek;
-
+    angularWeek.prototype.getStart = function (week) {
+      return this.startDate;
+    }
+    angularWeek.prototype.getEnd = function (week) {
+      return this.endDate;
+    }
+    
+    // Allow to get non singleton here
+    function create(date) {
+      return new angularWeek(date);
+    }
+    return {create:create};
   });
