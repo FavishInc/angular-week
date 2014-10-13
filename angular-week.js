a178ef537f9786410fbb51923bbbe272d179146a
Cole Pacak <cole@favish.com>
2014-10-13 12:50:28 -0500

    refactor

--- angular-week.js
+++ angular-week.js
@@ -8,30 +8,36 @@
  */
 angular.module('angularWeek', [])
   .factory('angularWeek', function () {
-    var angularWeek = function(date){
-       //this.setDate(date);
-    };
+    var angularWeek = function(){};
 
-    angularWeek.prototype.setStart = function (date) {
+    angularWeek.prototype.setDate = function (date) {
         this.startDate = moment(date).weekday(0);
         this.endDate   = moment(date).weekday(6);
     }
     angularWeek.prototype.next = function () {
-      return this.startDate.add(7, 'days');
+      return this.setDate(this.startDate.add(7, 'days'));
     }
     angularWeek.prototype.prev = function (week) {
-      return this.endDate.subtract(7, 'days');
+      return this.setDate(this.endDate.subtract(7, 'days'));
     }
-    angularWeek.prototype.getStart = function (week) {
-      return this.startDate;
-    }
-    angularWeek.prototype.getEnd = function (week) {
-      return this.endDate;
-    }
-    
     // Allow to get non singleton here
     function create(date) {
-      return new angularWeek(date);
+
+      // Set a default if none passed.
+      if(typeof date === 'undefined'){
+        date = moment.utc()
+      };
+
+      //Zero out the ours mins and secs
+      if(typeof date === 'moment') {
+        date = date.hours(0).minutes(0).seconds(0).milliseconds(0);
+      }
+
+      var week = new angularWeek(date);
+      week.setDate(date);
+      return week;
     }
-    return {create:create};
+    return {
+      create:create
+    };
   });
